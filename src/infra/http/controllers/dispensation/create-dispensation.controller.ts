9bd40fc840ba8adbfa9fda2abc51001953860967
Carlos Yuri <82901729+oyurisousa@users.noreply.github.com>
2025-08-04 08:01:50 -0300

    refactor: permissed dispense to SUPER ADMIN

--- src/infra/http/controllers/dispensation/create-dispensation.controller.ts
+++ src/infra/http/controllers/dispensation/create-dispensation.controller.ts
@@ -5,24 +5,22 @@ import {
   HttpCode,
   Post,
   UseGuards,
-} from '@nestjs/common';
-import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
-import { RolesGuard } from '@/infra/auth/roles.guard';
-import { Roles } from '@/infra/auth/role-decorator';
-import { ResourceNotFoundError } from '@/core/erros/errors/resource-not-found-error';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { DispensationMedicineUseCase } from '@/domain/pharma/application/use-cases/dispensation/dispensation-medicine';
-import { DispensationDto } from './dtos/dispensation-dto';
-import { CurrentUser } from '@/infra/auth/current-user-decorator';
-import { UserPayload } from '@/infra/auth/jwt-strategy';
-import { InstitutionNotExistsError } from '@/domain/pharma/application/use-cases/institution/_errors/institution-not-exists-error';
-import { OperatorRole } from '@/domain/pharma/enterprise/entities/operator';
+} from "@nestjs/common";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { RolesGuard } from "@/infra/auth/roles.guard";
+import { Roles } from "@/infra/auth/role-decorator";
+import { ResourceNotFoundError } from "@/core/erros/errors/resource-not-found-error";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { DispensationMedicineUseCase } from "@/domain/pharma/application/use-cases/dispensation/dispensation-medicine";
+import { DispensationDto } from "./dtos/dispensation-dto";
+import { CurrentUser } from "@/infra/auth/current-user-decorator";
+import { UserPayload } from "@/infra/auth/jwt-strategy";
+import { InstitutionNotExistsError } from "@/domain/pharma/application/use-cases/institution/_errors/institution-not-exists-error";
+import { OperatorRole } from "@/domain/pharma/enterprise/entities/operator";
 
-@ApiTags('dispensation')
+@ApiTags("dispensation")
 @ApiBearerAuth()
-@Controller('/dispensation')
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(OperatorRole.MANAGER, OperatorRole.COMMON)
+@Controller("/dispensation")
 export class DispensationController {
   constructor(private dispensation: DispensationMedicineUseCase) {}
 
@@ -30,7 +28,7 @@ export class DispensationController {
   @HttpCode(201)
   async handle(
     @CurrentUser() user: UserPayload,
-    @Body() body: DispensationDto,
+    @Body() body: DispensationDto
   ) {
     const { dispensationDate, patientId, medicines, stockId } = body;
     const result = await this.dispensation.execute({
--- src/infra/http/controllers/dispensation/dispensation-preview.controller.ts
+++ src/infra/http/controllers/dispensation/dispensation-preview.controller.ts
@@ -1,50 +1,51 @@
-import { BadRequestException, Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common'
-import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
-import { RolesGuard } from '@/infra/auth/roles.guard'
-import { Roles } from '@/infra/auth/role-decorator'
-import { ResourceNotFoundError } from '@/core/erros/errors/resource-not-found-error'
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
-import { OperatorRole } from '@/domain/pharma/enterprise/entities/operator'
-import { DispensationPreviewUseCase } from '@/domain/pharma/application/use-cases/dispensation/dispensation-preview'
-import { DispensationPreviewDto } from './dtos/dispensation-preview.dto'
-import { MedicineStockNotFoundError } from '@/domain/pharma/application/use-cases/stock/medicine-stock/_errors/medicine-stock-not-found-error'
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  Query,
+  UseGuards,
+} from "@nestjs/common";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { RolesGuard } from "@/infra/auth/roles.guard";
+import { Roles } from "@/infra/auth/role-decorator";
+import { ResourceNotFoundError } from "@/core/erros/errors/resource-not-found-error";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { OperatorRole } from "@/domain/pharma/enterprise/entities/operator";
+import { DispensationPreviewUseCase } from "@/domain/pharma/application/use-cases/dispensation/dispensation-preview";
+import { DispensationPreviewDto } from "./dtos/dispensation-preview.dto";
+import { MedicineStockNotFoundError } from "@/domain/pharma/application/use-cases/stock/medicine-stock/_errors/medicine-stock-not-found-error";
 
-@ApiTags('dispensation')
+@ApiTags("dispensation")
 @ApiBearerAuth()
-@Controller('/dispensation/preview')
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(OperatorRole.MANAGER, OperatorRole.COMMON)
+@Controller("/dispensation/preview")
 export class DispensationPreviewController {
-  constructor(
-    private dispensationPreview: DispensationPreviewUseCase,
-  ) {}
+  constructor(private dispensationPreview: DispensationPreviewUseCase) {}
 
   @Get()
   @HttpCode(200)
-  async handle(
-    @Query() queryParams: DispensationPreviewDto,
-  ) {
-    const { medicineStockId, quantityRequired } = queryParams
+  async handle(@Query() queryParams: DispensationPreviewDto) {
+    const { medicineStockId, quantityRequired } = queryParams;
     const result = await this.dispensationPreview.execute({
       medicineStockId,
       quantityRequired,
-    })
+    });
 
     if (result.isLeft()) {
-      const error = result.value
+      const error = result.value;
 
       switch (error.constructor) {
         case MedicineStockNotFoundError:
-          throw new ResourceNotFoundError()
+          throw new ResourceNotFoundError();
         default:
-          throw new BadRequestException(error.message)
+          throw new BadRequestException(error.message);
       }
     }
 
-    const { batchesPreview } = result.value
+    const { batchesPreview } = result.value;
 
     return {
       dispensationPreview: batchesPreview,
-    }
+    };
   }
 }
